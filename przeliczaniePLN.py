eab10852de2827c17168d0365649a80435bf65ae
Paweł_B <banasik.paul@gmail.com>
2022-02-27 11:32:51 +0100

    nadal nie dziala

--- przeliczaniePLN.py
+++ przeliczaniePLN.py
@@ -1,17 +1,6 @@
 def przeliczWaluta():
-    podajWaluta = input("Podaj waluta do przeliczenia:\n")
-    ilosc = input("Podaj ilość " + podajWaluta + "do przeliczenia na PLN \n")
-
-
-    waluta1 = "usd"
-
-
-    if podajWaluta() == waluta1:
-        wynik = int(ilosc) * 4.11
-        print(wynik)
-    else:
-        print("Error: Wybrano zla walute. Sproboj jeszcze raz.")
-        przeliczWaluta()
-
-
-przeliczWaluta()
+    print("podaj swoja ilosc pln:")
+    x = input()
+    print("podaj aktualny kurs:")
+    y = input()
+    print(y * x)
